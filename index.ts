f6667d8d28a04af93da408ea12b9f2bcd000122d
Dani Burnley <dan@danops.uk>
2023-07-03 15:00:30 +0100

    Update

--- index.ts
+++ index.ts
@@ -126,8 +126,8 @@ export class HmppsSessionStore extends Store {
     async function getRemoteSession(sessionId: string, serviceName: string, baseUrl: string) {
       try {
         const res = await axios.get(`${baseUrl}/${sessionId}/${serviceName}`)
-        console.log(res)
         centralSession = JSON.parse(res.data())
+        console.log(centralSession)
       } catch (e) {
         centralSession = {}
       }
